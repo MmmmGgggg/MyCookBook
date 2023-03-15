7e24f4ac10b58bbf224a89eabe8799c006aff8d1
ManjaG <manja.garaj@gmail.com>
2023-03-15 13:23:58 +0100

    first commit

---
+++ src/main/java/com/mgsoftware/MyCookBook/MyCookBookApplication.java
@@ -0,0 +1,13 @@
+package com.mgsoftware.MyCookBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCookBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCookBookApplication.class, args);
+	}
+
+}
